64c3494cba635ed070d2b6d5128c4d60d51d97f9
theSachinJadhav <sachin0321@gmail.com>
2018-02-04 15:44:33 +0530

    added JavaScript template files

---
+++ 04_External_JavaScript/script.js
@@ -0,0 +1,18 @@
+var green_p = document.getElementById("green-p");
+var myImage = document.getElementById("myImage");
+
+function blue() {
+    green_p.style.color = "blue";
+}
+
+function red() {
+    green_p.style.color = "red";
+}
+
+function fb() {
+    myImage.src = "../img/facebook.jpg";
+}
+
+function yt() {
+    myImage.src = "../img/youtube.jpg";
+}
